782c5460fdd7bed5e3ef41a0ec99ba0bcae6bead
Shubhi-Sri <shubhisrivastava514@gmail.com>
2025-09-17 18:32:09 +0000

    changes made

---
+++ automated-system-info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#automated system information
+#
+echo " System Info Report"
+echo "Hostname: $(hostname)"
+echo "Today is $(date)"
+if command -v lsb_release&> /dev/null; then
+        echo "CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')"
+else
+        echo "Operating System: $(uname -o)"
+fi
+echo "Kernel Version: $(uname -r)"
+echo "Uptime: $(uptime -p)"
+#
+echo "----------------------------------------------------------"
+echo "CPU Info"
+echo "CPU Cores: $(nproc)"
+echo "CPU Usage: $(top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}')%"
+#
+echo "----------------------------------------------------------"
+echo "Memory Info"
+echo "C Drive: $(df-h /mnt/c | awk 'NR==2 {print $3 " used / " $2 " total ($5" used)"}')"
+echo "D Drive: $(df-h /mnt/d | awk 'NR==2 {print $3 " used / " $2 " total ($5" used)"}')"
+#
+echo "----------------------------------------------------------"
+echo "Disk Usage $(df -h)"
+
+
